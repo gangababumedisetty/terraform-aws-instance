cae8c7d16f6b0274203b2e7d31d1bdf3a81aaf90
gangababu M <gangababumedisetty.com>
2026-03-09 15:30:28 +0530

    actions

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "instance_id" {
-    values  = aws_instance.this.id
+    value  = aws_instance.this.id
 }
 
 output "public_ip" {
-    values  = aws_instance.this.public_ip
+    value  = aws_instance.this.public_ip
 }
 
 output "private_ip" {
-    values  = aws_instance.this.private_ip
+    value = aws_instance.this.private_ip
 }
